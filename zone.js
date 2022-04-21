df7e313123d3b45936a7c5b2d7f48a637ccbacab
Norman-w <85535885@qq.com>
2022-04-21 13:47:52 +0800

    fix and and directly import dataSchema and extBuffer

--- zone.js
+++ zone.js
@@ -1,166 +1,235 @@
-var DataSchema = require("dataschema");
+var DataSchema = require("./dataschema");
 
 var ecoSchema = [
-    { name: "index",                type: "uint32" },
-    { name: "name",                 type: "nullstring" },
-    { name: "colorNxMap",           type: "nullstring" },
-    { name: "specBlendNyMap",       type: "nullstring" },
-    { name: "detailRepeat",         type: "uint32" },
-    { name: "blendStrength",        type: "float" },
-    { name: "specMin",              type: "float" },
-    { name: "specMax",              type: "float" },
-    { name: "specSmoothnessMin",    type: "float" },
-    { name: "specSmoothnessMax",    type: "float" },
-    { name: "physicsMaterial",      type: "nullstring" },
-    { name: "layers",               type: "array", fields: [
-        { name: "density",              type: "float" },
-        { name: "minScale",             type: "float" },
-        { name: "maxScale",             type: "float" },
-        { name: "slopePeak",            type: "float" },
-        { name: "slopeExtent",          type: "float" },
-        { name: "minElevation",         type: "float" },
-        { name: "maxElevation",         type: "float" },
-        { name: "minAlpha",             type: "uint8" },
-        { name: "flora",                type: "nullstring" },
-        { name: "tints",                type: "array", fields: [
-            { name: "color",                type: "rgba" },
-            { name: "percentage",           type: "uint32" }
+  { name: "index",                type: "uint32" },
+  { name: "name",                 type: "nullstring" },
+  { name: "colorNxMap",           type: "nullstring" },
+  { name: "specBlendNyMap",       type: "nullstring" },
+  { name: "detailRepeat",         type: "uint32" },
+  { name: "blendStrength",        type: "float" },
+  { name: "specMin",              type: "float" },
+  { name: "specMax",              type: "float" },
+  { name: "specSmoothnessMin",    type: "float" },
+  { name: "specSmoothnessMax",    type: "float" },
+  { name: "physicsMaterial",      type: "nullstring" },
+  { name: "layers",               type: "array", fields: [
+      { name: "density",              type: "float" },
+      { name: "minScale",             type: "float" },
+      { name: "maxScale",             type: "float" },
+      { name: "slopePeak",            type: "float" },
+      { name: "slopeExtent",          type: "float" },
+      { name: "minElevation",         type: "float" },
+      { name: "maxElevation",         type: "float" },
+      { name: "minAlpha",             type: "uint8" },
+      { name: "flora",                type: "nullstring" },
+      { name: "tints",                type: "array", fields: [
+          { name: "color",                type: "rgba" },
+          { name: "percentage",           type: "uint32" }
         ]}
     ]}
 ];
 
+
+
 var floraSchema = [
-    { name: "name",                 type: "nullstring" },
-    { name: "texture",              type: "nullstring" },
-    { name: "model",                type: "nullstring" },
-    { name: "unknownBoolean1",      type: "boolean" },
-    { name: "unknownFloat1",        type: "float" },
-    { name: "unknownFloat2",        type: "float" }
+  { name: "name",                 type: "nullstring" },
+  { name: "texture",              type: "nullstring" },
+  { name: "model",                type: "nullstring" },
+  { name: "unknownBoolean1",      type: "boolean" },
+  { name: "unknownFloat1",        type: "float" },
+  { name: "unknownFloat2",        type: "float" },
+  { name: "unknownFloat3",        type: "float" },
+  { name: "unknownFloat4",        type: "float" },
+  { name: "unknownFloat5",        type: "float" }
+];
+
+var floraSchemaPS2 = [
+  { name: "name",                 type: "nullstring" },
+  { name: "texture",              type: "nullstring" },
+  { name: "model",                type: "nullstring" },
+  { name: "unknownBoolean1",      type: "boolean" },
+  { name: "unknownFloat1",        type: "float" },
+  { name: "unknownFloat2",        type: "float" }
 ];
 
 var invisibleWallSchema = [
-    { name: "unknownUint32",        type: "uint32" },
-    { name: "unknownFloat1",        type: "float" },
-    { name: "unknownFloat2",        type: "float" },
-    { name: "unknownFloat3",        type: "float" }
+  { name: "unknownUint32",        type: "uint32" },
+  { name: "unknownFloat1",        type: "float" },
+  { name: "unknownFloat2",        type: "float" },
+  { name: "unknownFloat3",        type: "float" }
 ];
 
 var objectSchema = [
-    { name: "actorDefinition",      type: "nullstring" },
-    { name: "renderDistance",       type: "float" },
-    { name: "instances",            type: "array", fields: [
-        { name: "position",             type: "floatvector4" },
-        { name: "rotation",             type: "floatvector4" },
-        { name: "scale",                type: "floatvector4" },
-        { name: "id",                   type: "uint32" },
-        { name: "unknownByte1",         type: "uint8" },
-        { name: "unknownFloat1",        type: "float" }
+  { name: "actorDefinition",      type: "nullstring" },
+  { name: "renderDistance",       type: "float" },
+  { name: "instances",            type: "array", fields: [
+      { name: "position",             type: "floatvector4" },
+      { name: "rotation",             type: "floatvector4" },
+      { name: "scale",                type: "floatvector4" },
+      { name: "id",                   type: "uint32" },
+      { name: "unknownByte1",         type: "uint8" },
+      { name: "unknownFloat1",        type: "float" }
     ]}
 ];
 
 var lightSchema = [
-    { name: "name",                 type: "nullstring" },
-    { name: "colorName",            type: "nullstring" },
-    { name: "type",                 type: "uint8" },
-    { name: "unknownFloat1",        type: "float" },
-    { name: "position",             type: "floatvector4" },
-    { name: "rotation",             type: "floatvector4" },
-    { name: "range",                type: "float" },
-    { name: "innerRange",           type: "float" },
-    { name: "color",                type: "argb" },
-    { name: "unknownByte1",         type: "uint8" },
-    { name: "unknownByte2",         type: "uint8" },
-    { name: "unknownByte3",         type: "uint8" },
-    { name: "unknownByte4",         type: "uint8" },
-    { name: "unknownByte5",         type: "uint8" },
-    { name: "unknownVector1",       type: "floatvector4" },
-    { name: "unknownString1",       type: "nullstring" },
-    { name: "id",                   type: "uint32" }
+  { name: "name",                 type: "nullstring" },
+  { name: "colorName",            type: "nullstring" },
+  { name: "type",                 type: "uint8" },
+  { name: "unknownFloat1",        type: "float" },
+  { name: "position",             type: "floatvector4" },
+  { name: "rotation",             type: "floatvector4" },
+  { name: "range",                type: "float" },
+  { name: "innerRange",           type: "float" },
+  { name: "color",                type: "argb" },
+  { name: "unknownByte1",         type: "uint8" },
+  { name: "unknownByte2",         type: "uint8" },
+  { name: "unknownByte3",         type: "uint8" },
+  { name: "unknownByte4",         type: "uint8" },
+  { name: "unknownByte5",         type: "uint8" },
+  { name: "unknownVector1",       type: "floatvector4" },
+  { name: "unknownString1",       type: "nullstring" },
+  { name: "id",                   type: "uint32" }
+];
+
+
+var decalSchema = [
+  { name: "unknownFloat1",        type: "float" },
+  { name: "position",             type: "floatvector4" },
+  { name: "unknownFloat2",        type: "float" },
+  { name: "unknownFloat3",        type: "float" },
+  { name: "unknownFloat4",        type: "float" },
+  { name: "unknownFloat5",        type: "float" },
+  { name: "decimalDigits6And4",   type: "uint32" }, //I mean, uh, the last 4 digits in decimal seem to be similar or same for several values, thus probably have some significance
+  { name: "name",                 type: "nullstring" },
+  { name: "unknownFloat6",        type: "float" },
+  { name: "unknownFloat7",        type: "float" },
+  { name: "unknownFloat8",        type: "float" },
+  { name: "unknownInt",           type: "uint32" }
 ];
 
 var schema = [
-    { name: "signature",            type: "uint32" },
-    { name: "version",              type: "uint32" },
-    { name: "offsets",              type: "schema", fields: [
-        { name: "ecos",                 type: "uint32" },
-        { name: "floras",               type: "uint32" },
-        { name: "invisibleWalls",       type: "uint32" },
-        { name: "objects",              type: "uint32" },
-        { name: "lights",               type: "uint32" },
-        { name: "unknowns",             type: "uint32" }
+  { name: "signature",            type: "uint32" },
+  { name: "version",              type: "uint32" },
+  { name: "unknown",              type: "uint32" },
+  { name: "offsets",              type: "schema", fields: [
+      { name: "ecos",                 type: "uint32" },
+      { name: "floras",               type: "uint32" },
+      { name: "invisibleWalls",       type: "uint32" },
+      { name: "objects",              type: "uint32" },
+      { name: "lights",               type: "uint32" },
+      { name: "unknowns",             type: "uint32" },
+      { name: "decals",               type: "uint32" }
     ]},
-    { name: "quadsPerTile",         type: "uint32" },
-    { name: "tileSize",             type: "float" },
-    { name: "tileHeight",           type: "float" },
-    { name: "vertsPerTile",         type: "uint32" },
-    { name: "tilesPerChunk",        type: "uint32" },
-    { name: "startX",               type: "int32" },
-    { name: "startY",               type: "int32" },
-    { name: "chunksX",              type: "uint32" },
-    { name: "chunksY",              type: "uint32" },
-    { name: "ecos",                 type: "array", fields: ecoSchema },
-    { name: "floras",               type: "array", fields: floraSchema },
-    { name: "invisibleWalls",       type: "array", fields: invisibleWallSchema },
-    { name: "objects",              type: "array", fields: objectSchema },
-    { name: "lights",               type: "array", fields: lightSchema },
-    { name: "unknowns",             type: "array", fields: [] }
+  { name: "quadsPerTile",         type: "uint32" },
+  { name: "tileSize",             type: "float" },
+  { name: "tileHeight",           type: "float" },
+  { name: "vertsPerTile",         type: "uint32" },
+  { name: "tilesPerChunk",        type: "uint32" },
+  { name: "startX",               type: "int32" },
+  { name: "startY",               type: "int32" },
+  { name: "chunksX",              type: "uint32" },
+  { name: "chunksY",              type: "uint32" },
+  { name: "ecos",                 type: "array", fields: ecoSchema },
+  { name: "floras",               type: "array", fields: floraSchema },
+  { name: "invisibleWalls",       type: "array", fields: invisibleWallSchema },
+  { name: "objects",              type: "array", fields: objectSchema },
+  { name: "lights",               type: "array", fields: lightSchema },
+  { name: "unknowns",             type: "array", fields: [] },
+  { name: "decals",               type: "array", fields: decalSchema }
+
+];
+
+var schemaPS2 = [
+  { name: "signature",            type: "uint32" },
+  { name: "version",              type: "uint32" },
+  { name: "offsets",              type: "schema", fields: [
+      { name: "ecos",                 type: "uint32" },
+      { name: "floras",               type: "uint32" },
+      { name: "invisibleWalls",       type: "uint32" },
+      { name: "objects",              type: "uint32" },
+      { name: "lights",               type: "uint32" },
+      { name: "unknowns",             type: "uint32" }
+    ]},
+  { name: "quadsPerTile",         type: "uint32" },
+  { name: "tileSize",             type: "float" },
+  { name: "tileHeight",           type: "float" },
+  { name: "vertsPerTile",         type: "uint32" },
+  { name: "tilesPerChunk",        type: "uint32" },
+  { name: "startX",               type: "int32" },
+  { name: "startY",               type: "int32" },
+  { name: "chunksX",              type: "uint32" },
+  { name: "chunksY",              type: "uint32" },
+  { name: "ecos",                 type: "array", fields: ecoSchema },
+  { name: "floras",               type: "array", fields: floraSchemaPS2 },
+  { name: "invisibleWalls",       type: "array", fields: invisibleWallSchema },
+  { name: "objects",              type: "array", fields: objectSchema },
+  { name: "lights",               type: "array", fields: lightSchema },
+  { name: "unknowns",             type: "array", fields: [] }
 
 ];
 
 
 function readZone(data, offset) {
-    offset = offset || 0;
+  offset = offset || 0;
 
-    var signature = data.readUInt32LE(offset);
-    if (signature != 0x454E4F5A) {
-        throw "Not a valid zone file";
-    }
-
+  var signature = data.readUInt32LE(offset);
+  if (signature != 0x454E4F5A) {
+    throw "Not a valid zone file";
+  }
+  var version = data.readUInt32LE(offset+4);
+  if (version == 0x00000001) {
+    var zone = DataSchema.parse(schemaPS2, data, offset);
+  }
+  else {
+    var fc = data.readNullTerminatedString;
     var zone = DataSchema.parse(schema, data, offset);
-    return zone.result;
+  }
+
+  return zone.result;
 
 }
 
 function writeZone(zone) {
-    // calculate new offsets
-    var offset = 68, i;
-    
-    zone.offsets.ecos = offset;
+  // calculate new offsets
+  var offset = 68, i;
 
-    offset += 4;
-    for (i=0;i<zone.ecos.length;i++) {
-        offset += DataSchema.sizeOf(ecoSchema, zone.ecos[i]);
-    }
-    zone.offsets.floras = offset;
+  zone.offsets.ecos = offset;
 
-    offset += 4;
-    for (i=0;i<zone.floras.length;i++) {
-        offset += DataSchema.sizeOf(floraSchema, zone.floras[i]);
-    }
-    zone.offsets.invisibleWalls = offset;
+  offset += 4;
+  for (i=0;i<zone.ecos.length;i++) {
+    offset += DataSchema.sizeOf(ecoSchema, zone.ecos[i]);
+  }
+  zone.offsets.floras = offset;
 
-    offset += 4;
-    for (i=0;i<zone.invisibleWalls.length;i++) {
-        offset += DataSchema.sizeOf(invisibleWallSchema, zone.invisibleWalls[i]);
-    }
-    zone.offsets.objects = offset;
+  offset += 4;
+  for (i=0;i<zone.floras.length;i++) {
+    offset += DataSchema.sizeOf(floraSchema, zone.floras[i]);
+  }
+  zone.offsets.invisibleWalls = offset;
 
-    offset += 4;
-    for (i=0;i<zone.objects.length;i++) {
-        offset += DataSchema.sizeOf(objectSchema, zone.objects[i]);
-    }
-    zone.offsets.lights = offset;
+  offset += 4;
+  for (i=0;i<zone.invisibleWalls.length;i++) {
+    offset += DataSchema.sizeOf(invisibleWallSchema, zone.invisibleWalls[i]);
+  }
+  zone.offsets.objects = offset;
 
-    offset += 4;
-    for (i=0;i<zone.lights.length;i++) {
-        offset += DataSchema.sizeOf(lightSchema, zone.lights[i]);
-    }
-    zone.offsets.unknowns = offset;
+  offset += 4;
+  for (i=0;i<zone.objects.length;i++) {
+    offset += DataSchema.sizeOf(objectSchema, zone.objects[i]);
+  }
+  zone.offsets.lights = offset;
 
-    // write data
-    var result = DataSchema.pack(schema, zone);
+  offset += 4;
+  for (i=0;i<zone.lights.length;i++) {
+    offset += DataSchema.sizeOf(lightSchema, zone.lights[i]);
+  }
+  zone.offsets.unknowns = offset;
 
-    return result.data;
+  // write data
+  var result = DataSchema.pack(schemaH1Z1, zone);
+
+  return result.data;
 }
 
 exports.read = readZone;
--- zonetool.js
+++ zonetool.js
@@ -1,151 +1,154 @@
 #!/usr/bin/env node
-
+const ExtBuffer = require('./extbuffer');
 var Zone = require("./zone.js"),
-    fs = require("fs"),
-    path = require("path"),
-    VERSION = "0.1.0";
-    
-console.log();  
+  fs = require("fs"),
+  path = require("path"),
+  VERSION = "0.1.0";
+
+console.log();
 console.log("Forgelight Zone Tool " + VERSION);
 console.log();
 
 function usage() {
-    console.log("Usage: ./zonetool.js <mode> <inPath> [<outPath>]");
-    console.log();
-    console.log("Modes: json        Export Zone data to JSON");
-    console.log("       info        Print general information about Zone data");
-    console.log("       write       Write JSON data to .zone file");
-    console.log("       test        Test tool integrity on zone file <inpath> ");
+  console.log("Usage: ./zonetool.js <mode> <inPath> [<outPath>]");
+  console.log();
+  console.log("Modes: json        Export Zone data to JSON");
+  console.log("       info        Print general information about Zone data");
+  console.log("       write       Write JSON data to .zone file");
+  console.log("       test        Test tool integrity on zone file <inpath> ");
 
 }
-    
+
 var mode = process.argv[2],
-    inPath = process.argv[3],
-    outPath = process.argv[4],
-    zone, json;
-    
+  inPath = process.argv[3],
+  outPath = process.argv[4],
+  zone, json;
+
 
 switch (mode) {
-    case "json":
-        if (inPath) {
-            if (!fs.existsSync(inPath)) {
-                throw "inPath does not exist";
-            }
-            var data = fs.readFileSync(inPath);
-            zone = Zone.read(data);
-            console.log("Converting ZONE to JSON...");
-            if (!outPath) {
-                outPath = process.argv[4];
-                if (outPath) {
-                    if (fs.existsSync(outPath)) {
-                        if (fs.statSync(outPath).isDirectory()) {
-                            outPath += path.basename(inPath, ".zone") + ".json";
-                        } else {
-                            throw "File already exists: " + outPath;
-                        }
-                    }
-                } else {
-                    outPath = path.join(path.dirname(inPath), path.basename(inPath, ".zone") + ".json");
-                }
-            }
-            console.log("Writing data to file:", outPath);
-            json = JSON.stringify(zone, null, 4);
-            fs.writeFileSync(outPath, json);
-            console.log("Done.");
-        } else {
-            usage();
-        }
-        break;
-    case "write":
-        if (inPath) {
-            if (!fs.existsSync(inPath)) {
-                throw "inPath does not exist";
-            }
-            var zone = JSON.parse(fs.readFileSync(inPath));
-            console.log("Converting JSON to ZONE...");
-            if (!outPath) {
-                outPath = process.argv[4];
-                if (outPath) {
-                    if (fs.existsSync(outPath)) {
-                        if (fs.statSync(outPath).isDirectory()) {
-                            outPath += path.basename(inPath, ".json") + ".zone";
-                        } else {
-                            throw "File already exists: " + outPath;
-                        }
-                    }
-                } else {
-                    outPath = path.join(path.dirname(inPath), path.basename(inPath, ".json") + ".zone");
-                }
-            }
-            console.log("Writing data to file:", outPath);
-            var data = Zone.write(zone);
-            fs.writeFileSync(outPath, data);
-            console.log("Done.");
-        } else {
-            usage();
-        }
-        break;
-    case "test":
-        if (inPath) {
-            if (!fs.existsSync(inPath)) {
-                throw "inPath does not exist";
-            }
-            var data = fs.readFileSync(inPath);
-            zone = Zone.read(data);
-            var data2 = Zone.write(zone);
-            
-            if (data.length == data2.length) {
-                for (var i=0,j=data.length;i<j;i++) {
-                    if (data[i] !== data2[i]) {
-                        fs.writeFileSync("test1.dat", data);
-                        fs.writeFileSync("test2.dat", data2);
-
-                        console.log("Data mismatch at offset " + i);
-                        break;
-                    }
-                }
-                if (i == data.length) {
-                    console.log("Integrity test passed!");
-                }
+  case "json":
+    if (inPath) {
+      if (!fs.existsSync(inPath)) {
+        throw "inPath does not exist";
+      }
+      var data = fs.readFileSync(inPath);
+      let dd = new ExtBuffer(data, 0, data.length);
+      zone = Zone.read(dd);
+      console.log("Converting ZONE to JSON...");
+      if (!outPath) {
+        outPath = process.argv[4];
+        if (outPath) {
+          if (fs.existsSync(outPath)) {
+            if (fs.statSync(outPath).isDirectory()) {
+              outPath += path.basename(inPath, ".zone") + ".json";
             } else {
-                fs.writeFileSync("test1.dat", data);
-                fs.writeFileSync("test2.dat", data2);
-                console.log("Data length mismatch: " + data.length + " != " + data2.length);
+              throw "File already exists: " + outPath;
             }
+          }
         } else {
-            usage();
+          outPath = path.join(path.dirname(inPath), path.basename(inPath, ".zone") + ".json");
         }
-        break;
-    case "info":
-        if (inPath) {
-            if (!fs.existsSync(inPath)) {
-                throw "inPath does not exist";
+      }
+      console.log("Writing data to file:", outPath);
+      json = JSON.stringify(zone, null, 4);
+      fs.writeFileSync(outPath, json);
+      console.log("Done.");
+    } else {
+      usage();
+    }
+    break;
+  case "write":
+    if (inPath) {
+      if (!fs.existsSync(inPath)) {
+        throw "inPath does not exist";
+      }
+      var zone = JSON.parse(fs.readFileSync(inPath));
+      console.log("Converting JSON to ZONE...");
+      if (!outPath) {
+        outPath = process.argv[4];
+        if (outPath) {
+          if (fs.existsSync(outPath)) {
+            if (fs.statSync(outPath).isDirectory()) {
+              outPath += path.basename(inPath, ".json") + ".zone";
+            } else {
+              throw "File already exists: " + outPath;
             }
-            var data = fs.readFileSync(inPath);
-            zone = Zone.read(data);
-            console.log("Zone Info: " + path.basename(inPath));
-            console.log();
-            console.log("quadsPerTile:      ", zone.quadsPerTile);
-            console.log("tileSize:          ", zone.tileSize);
-            console.log("tileHeight:        ", 1/zone.tileHeight);
-            console.log("vertsPerTile:      ", zone.vertsPerTile);
-            console.log("tilesPerChunk:     ", zone.tilesPerChunk);
-            console.log("startX:            ", zone.startX);
-            console.log("startY:            ", zone.startY);
-            console.log("chunksX:           ", zone.chunksX);
-            console.log("chunksY:           ", zone.chunksY);
-            console.log("ECOs:              ", zone.ecos.length);
-            console.log("Floras:            ", zone.floras.length);
-            console.log("Invisible Walls:   ", zone.invisibleWalls.length);
-            console.log("Object Instances:  ", zone.objects.length);
-            console.log("Lights:            ", zone.lights.length);
+          }
         } else {
-            usage();
+          outPath = path.join(path.dirname(inPath), path.basename(inPath, ".json") + ".zone");
         }
-        break;
-    default: 
-        console.log("Unknown mode:", mode);
-        usage();
+      }
+      console.log("Writing data to file:", outPath);
+      var data = Zone.write(zone);
+      fs.writeFileSync(outPath, data);
+      console.log("Done.");
+    } else {
+      usage();
+    }
+    break;
+  case "test":
+    if (inPath) {
+      if (!fs.existsSync(inPath)) {
+        throw "inPath does not exist";
+      }
+      var data = fs.readFileSync(inPath);
+      let dd = new ExtBuffer(data, 0, data.length);
+      zone = Zone.read(dd);
+      var data2 = Zone.write(zone);
+
+      if (data.length == data2.length) {
+        for (var i = 0, j = data.length; i < j; i++) {
+          if (data[i] !== data2[i]) {
+            fs.writeFileSync("test1.dat", data);
+            fs.writeFileSync("test2.dat", data2);
+
+            console.log("Data mismatch at offset " + i);
+            break;
+          }
+        }
+        if (i == data.length) {
+          console.log("Integrity test passed!");
+        }
+      } else {
+        fs.writeFileSync("test1.dat", data);
+        fs.writeFileSync("test2.dat", data2);
+        console.log("Data length mismatch: " + data.length + " != " + data2.length);
+      }
+    } else {
+      usage();
+    }
+    break;
+  case "info":
+    if (inPath) {
+      if (!fs.existsSync(inPath)) {
+        throw "inPath does not exist";
+      }
+      var data = fs.readFileSync(inPath);
+      let dd = new ExtBuffer(data, 0, data.length);
+      zone = Zone.read(dd);
+      console.log("Zone Info: " + path.basename(inPath));
+      console.log();
+      console.log("quadsPerTile:      ", zone.quadsPerTile);
+      console.log("tileSize:          ", zone.tileSize);
+      console.log("tileHeight:        ", 1 / zone.tileHeight);
+      console.log("vertsPerTile:      ", zone.vertsPerTile);
+      console.log("tilesPerChunk:     ", zone.tilesPerChunk);
+      console.log("startX:            ", zone.startX);
+      console.log("startY:            ", zone.startY);
+      console.log("chunksX:           ", zone.chunksX);
+      console.log("chunksY:           ", zone.chunksY);
+      console.log("ECOs:              ", zone.ecos.length);
+      console.log("Floras:            ", zone.floras.length);
+      console.log("Invisible Walls:   ", zone.invisibleWalls.length);
+      console.log("Object Instances:  ", zone.objects.length);
+      console.log("Lights:            ", zone.lights.length);
+    } else {
+      usage();
+    }
+    break;
+  default:
+    console.log("Unknown mode:", mode);
+    usage();
 }
 
 var f = new Buffer(4);
